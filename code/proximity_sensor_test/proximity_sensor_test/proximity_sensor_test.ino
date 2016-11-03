cce15bc66ef84d5174c6f4a07bc72bf42ca50c6b
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2016-11-03 17:42:03 +0100

    Adds simple Arduino test program used to verify that the proximity sensors works as expected.

--- code/proximity_sensor_test/proximity_sensor_test/proximity_sensor_test.ino
+++ code/proximity_sensor_test/proximity_sensor_test/proximity_sensor_test.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
-
+int proximity_sensor_pin = 7;
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(proximity_sensor_pin, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  if ( digitalRead(proximity_sensor_pin) )
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  
 }
